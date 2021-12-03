d67d3d1291ffc61ff57cd4107639d0f1c5f27331
Frown00 <fabian.kuriata.dev@gmail.com>
2021-12-03 19:39:27 +0100

    feat(2021-03_day): complete solution part 1

---
+++ 2021/03_day/Binary Diagnostic 1/solution.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+function solvePuzzle(puzzlePath) {
+  const content = readContent(puzzlePath);
+  const binaryCodes = content.split(/\r?\n/);
+  const occurancesCount = [];
+  for(let binary of binaryCodes) {
+    const bits = binary.split('');
+    for(let i = 0; i < bits.length; i++) {
+      const bit = bits[i];
+      occurancesCount[i] ? occurancesCount[i][bit]++ : occurancesCount[i] = { 1: 0, 0: 0}
+    }
+  }
+  const gammaReducer = (previousValue, currentValue) => {
+    // 0 > 1
+    if(currentValue?.['0'] > currentValue?.['1']) 
+      return previousValue + '0';
+    // 1 > 0
+    return previousValue + '1';
+
+  };
+  const gammaRate = occurancesCount.reduce(gammaReducer, '');
+  const epsilonRate = gammaRate.split('').map(n => n === '1' ? '0' : '1').join('');
+  const gammaRateInDecimal = parseInt(gammaRate, 2);
+  const epsilonRateInDecimal = parseInt(epsilonRate, 2);
+  return gammaRateInDecimal * epsilonRateInDecimal;
+}
+
+function readContent(path) {
+  return fs.readFileSync(path, 'utf-8', (err, data) => {
+    if(err) {
+      return console.log(err);
+    }
+  })
+}
+
+const solution = solvePuzzle('input.txt');
+console.log(solution);
