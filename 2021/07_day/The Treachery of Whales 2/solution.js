19b292c26d64edec07259a7123cc58a32a186c66
Fabian Kuriata <fabian.kuriata.dev@gmail.com>
2021-12-12 02:06:47 +0100

    feat(2021-07_day): complete solution of the treachery of whales part two

---
+++ 2021/07_day/The Treachery of Whales 2/solution.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+
+function calcMedian(arr) {
+  const middle = Math.floor(arr.length / 2);
+  if(arr.length % 2 === 0) {
+    const middle2 = middle - 1;
+    return (arr[middle] + arr[middle2]) / 2
+  }
+  return arr[middle];
+}
+
+function calcAvg(arr) {
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+
+function routeFuelCost(pointA, pointB) {
+  const distance = Math.abs(pointA - pointB);
+  let cost = 0;
+  for(let i = 1; i <= distance; i++) {
+    cost += i;
+  }
+  return cost;
+}
+
+function calcFuelConsumtion(crabs, position) {
+  let fuelConsumed = 0;
+  for(let i = 0; i < crabs.length; i++) {
+    fuelConsumed += routeFuelCost(position, crabs[i])
+  }
+  return fuelConsumed;
+}
+
+function solvePuzzle(puzzlePath) {
+  const content = readContent(puzzlePath);
+  const crabs = content.split(',').map(n => parseInt(n)).sort((a, b) => a - b);
+  const position = Math.floor(calcAvg(crabs));
+  const fuelConsumed = calcFuelConsumtion(crabs, position);
+  return fuelConsumed;
+}
+
+function readContent(path) {
+  return fs.readFileSync(path, "utf-8", (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+  });
+}
+
+const solution = solvePuzzle("input.txt");
+console.log(solution);
