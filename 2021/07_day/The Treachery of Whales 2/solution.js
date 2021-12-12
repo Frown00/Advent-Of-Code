ab5092e8e3f9be1f6c3401a4493b3eade516330f
Fabian Kuriata <fabian.kuriata.dev@gmail.com>
2021-12-12 02:23:27 +0100

    fix(2021-07_day): remove unused median function

--- 2021/07_day/The Treachery of Whales 2/solution.js
+++ 2021/07_day/The Treachery of Whales 2/solution.js
@@ -1,14 +1,5 @@
 const fs = require("fs");
 
-function calcMedian(arr) {
-  const middle = Math.floor(arr.length / 2);
-  if(arr.length % 2 === 0) {
-    const middle2 = middle - 1;
-    return (arr[middle] + arr[middle2]) / 2
-  }
-  return arr[middle];
-}
-
 function calcAvg(arr) {
   let sum = 0;
   for(let i = 0; i < arr.length; i++) {
